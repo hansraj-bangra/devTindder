992bad905cb7d4b88560e4cb415d2a9c53aff817
hansraj <condingwithhansu44@gmail.com>
2026-05-20 22:52:28 +0530

    get post commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,20 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("Hello Node js!!!!");
-});
+app.get("/user",(req,res) =>{
+    res.send({name:"hansraj",age:20})
+})
+
+
+app.post("/user",(req,res) =>{
+   console.log(req.body)
+   res.send("Data successfully saved to Database!")
+})
+app.use("/",(req,res)=>{
+    res.send("Hello test!!!!!!!")
+})
+
+
+
 
 app.listen(3000,()=>console.log("server is running...!!!"))
