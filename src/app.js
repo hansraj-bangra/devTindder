b6a95aa1acdcd3a98e4ec312e7dfca8ba9e62c84
hansraj <condingwithhansu44@gmail.com>
2026-05-23 23:17:27 +0530

    Connection database

--- src/app.js
+++ src/app.js
@@ -1,24 +1,45 @@
 const express = require("express");
+const connectDB = require("./config/database");
+const User = require("./models/user");
 
 const app = express();
 
 app.use(express.json());
 
-
-app.use("/",(err,req,res,next)=>{
-    if(err){
-        res.status(500).send("Something went wrong..!!!")
+app.post("/signup",async (req,res)=>{
+    console.log(req.body );
+    const user = new User(req.body)
+    try{
+        await user.save()
+    res.status(201).send(req.body)
+    }catch(err){
+        res.status(400).send("Something went wrong in user detail!!")
     }
+
 })
 
-app.get("/user",(req,res,next) =>{
-    console.log("Handling the route user!")
-    // res.send("Response!!")
-    next();
-},(req,res)=>{
-    console.log("Handling the route user 2!!")
-    res.send("2nd Response!!")
+connectDB().then(()=>{
+    console.log("Database connection established!!!");
+    app.listen(3000,()=>console.log("server is running...!!!"))
+}).catch(err =>{
+    console.error("Database connection failed!!!")
 })
+
+
+// app.use("/",(err,req,res,next)=>{
+//     if(err){
+//         res.status(500).send("Something went wrong..!!!")
+//     }
+// })
+
+// app.get("/user",(req,res,next) =>{
+//     console.log("Handling the route user!")
+//     // res.send("Response!!")
+//     next();
+// },(req,res)=>{
+//     console.log("Handling the route user 2!!")
+//     res.send("2nd Response!!")
+// })
 
 
 // app.post("/user",(req,res) =>{
@@ -29,8 +50,3 @@ app.get("/user",(req,res,next) =>{
 //     res.send("Hello test!!!!!!!")
 // })
 
-
-
-
-
-app.listen(3000,()=>console.log("server is running...!!!"))
