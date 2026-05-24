b376e0b6542f0c09d868339a95fbd65e01177d40
hansraj <condingwithhansu44@gmail.com>
2026-05-24 17:11:18 +0530

    API validation and data senitization

--- src/app.js
+++ src/app.js
@@ -6,16 +6,60 @@ const app = express();
 
 app.use(express.json());
 
+app.patch("/user/:userId",async(req,res)=>{
+    const userId =req.params.userId;
+    const data = req.body;
+    try {
+        const ALLOWED_UPDATES = ["photoUrl","about","gender","age","skills"];
+        const  isUpdateAllowed = Object.keys(data).every((k) =>ALLOWED_UPDATES.includes(k));
+        if(!isUpdateAllowed){
+            throw new Error("Update not allowed");
+        }
+
+        if(data.skills.length >10){
+            throw new Error("Skills not allowed more than 10")
+        }
+        const user = await User.findByIdAndUpdate({_id:userId},data,{returnDocument:"after",runValidator:true});
+        console.log(user);
+        res.send("User updated successfully!!")
+    } catch (error) {
+        res.status(400).send("UPDATED FAILED "+ err.message)
+    }
+})
+
 app.post("/signup",async (req,res)=>{
     console.log(req.body );
     const user = new User(req.body)
     try{
-        await user.save()
+        await user.save();
     res.status(201).send(req.body)
     }catch(err){
         res.status(400).send("Something went wrong in user detail!!")
     }
 
+});
+
+app.get("/user",async (req,res)=>{
+    const email = req.boby.emailId;
+    const user = await User.findOne({emailId:emailId});
+    try {
+        if(!user){
+            res.status(400).send("Something went wrong in user detail!!")
+        }
+        res.send(user)
+    } catch (error) {
+        res.status(400).send("Something went wrong in user detail!!")
+    }
+    
+})
+
+app.get("/feed", async(req,res)=>{
+    try {
+        const users = await User.find({});
+        res.send(users)
+    } catch (error) {
+        res.status(400).send("Something went wrong in user detail!!")
+    }
 })
 
 connectDB().then(()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -2,24 +2,46 @@ const mongoose = require("mongoose")
 
 const userSchema = mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        minLength:4,
+        maxLength:50
     },
     lastName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        require:true,
+        unique:true,
+        trim:true
     },
     password:{
-        type:String
+        type:String,
+        required:true
     },
     age:{
-        type:Number
+        type:Number,
+        min:18
+        
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender data is not valid!!")
+            }
+        }
+    },
+    photoUrl:{
+        type:String,
+        default:"https://media.istockphoto.com/id/1327592506/vector/default-avatar-photo-placeholder-icon-grey-profile-picture-business-man.jpg?s=1024x1024&w=is&k=20&c=er-yFBCv5wYO_curZ-MILgW0ECSjt0DDg5OlwpsAgZM="
+    },
+    about:{
+        type:String,
+        default:"This is a default about of the user!"
     }
-});
+},{timestamp:true});
 
 
 module.exports = mongoose.model("User",userSchema);
